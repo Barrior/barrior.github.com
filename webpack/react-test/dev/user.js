8abcc4e443022d061cf44631c277b7a29b80448a
barrior <barrior@qq.com>
2016-09-23 11:16:04 +0800

    learn webpack

---
+++ webpack/react-test/dev/user.js
@@ -0,0 +1,16 @@
+/**
+ * Created by weid on 2016/9/20.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/app.less';
+
+
+let container = document.createElement( 'div' );
+document.body.appendChild( container );
+
+ReactDOM.render(
+    '<div>用户中心</div>',
+    container
+);

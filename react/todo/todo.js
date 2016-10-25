da9f7458e936f4a45a263edd162b911ba030efc2
barrior <barrior@qq.com>
2016-10-25 20:47:04 +0800

    todo

---
+++ react/todo/todo.js
@@ -0,0 +1,12 @@
+/**
+ * Created by weid on 2016/10/25.
+ */
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import remarkable from 'remarkable';
+import './todo.less';
+
+ReactDOM.render(
+    <Todo />,
+    document.getElementById('app')
+);

289f37c37c3c3596b5cc99ea80a153d478057d2d
barrior <barrior@qq.com>
2017-06-16 18:12:59 +0800

    save

--- react/redux_todos/redux_todos.js
+++ react/redux_todos/redux_todos.js
@@ -1,7 +1,22 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import {createStrore, combineReducers, applyMiddleware} from 'redux';
+import {createStrore, combineReducers, applyMiddleware, bindActionCreators, compose} from 'redux';
 import './todos.less';
+
+/*
+ createStrore: 创建一个 Redux store 来以存放应用中所有的 state
+ combineReducers:
+ applyMiddleware:
+ bindActionCreators:
+ compose:
+
+ const store = createStrore(reducer);
+ store
+    .getState()
+    .dispatch(action)
+    .subscribe(listener)
+    .replaceReducer(nextReducer)
+ */
 
 class Todo extends Component {
     constructor(props) {

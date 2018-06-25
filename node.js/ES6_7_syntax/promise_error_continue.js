58cd6b8c324c044bbbd0dc0c9a35db059b238281
Barrior <barrior@qq.com>
2018-06-25 09:57:36 +0800

    how to catch promise reject correctly

--- node.js/ES6_7_syntax/promise_error_continue.js
+++ node.js/ES6_7_syntax/promise_error_continue.js
@@ -3,16 +3,19 @@ let getData = function () {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('before reject');
-            reject('error');
-          console.log('after reject');
+            reject(new Error('***** error *******'));
+            console.log('after reject');
         }, 1000);
     });
 };
 
-getData()
-    .then(msg => {
-        console.log('then: ', msg);
-    })
-    .catch(err => {
-        console.log('catch: ', err);
-    })
+;(async function start () {
+  try {
+    const { stderr, stdout } = await getData()
+    console.log('getData completed.', stderr, stdout)
+  } catch (err) {
+    console.log('\n ++++ in catch: ++++ \n')
+    console.log('err: %o \n', err)
+    console.log('typeof err: %s', typeof err)
+  }
+}())

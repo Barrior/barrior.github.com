53e2c7101c78442051c1f2b3f48251946e8d7597
Barrior <barrior@qq.com>
2018-08-23 10:56:00 +0800

    Feat: improve utilities

---
+++ utilities/dom.js
@@ -0,0 +1,97 @@
+// check if the element be contained in the container
+// result that equal jQuery.contains
+export function contains (container, target) {
+  if (target) {
+    while ((target = target.parentNode)) {
+      if (container === target) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+export function removeElement (selector) {
+  const elem = document.querySelector(selector)
+  elem && elem.parentNode.removeChild(elem)
+}
+
+export function isWindow (elem) {
+  return elem != null && elem === elem.window
+}
+
+export function offset (elem) {
+  const offset = { left: 0, top: 0 }
+  if (!isWindow(elem) && elem.nodeType !== 9) {
+    while (elem) {
+      offset.left += elem.offsetLeft
+      offset.top += elem.offsetTop
+      elem = elem.offsetParent
+    }
+  }
+  return offset
+}
+
+export function getClientHeight (elem) {
+  if (isWindow(elem) || elem.nodeType === 9) {
+    elem = document.documentElement
+  }
+  return elem.clientHeight
+}
+
+export function getScrollHeight (elem) {
+  if (isWindow(elem) || elem.nodeType === 9) {
+    elem = document.documentElement
+  }
+  return elem.scrollHeight
+}
+
+export function scrollTop (elem, value) {
+  let win
+  if (isWindow(elem)) {
+    win = elem
+  } else if (elem.nodeType === 9) {
+    win = elem.defaultView
+  }
+
+  if (value === undefined) {
+    return win ? win.pageYOffset : elem.scrollTop
+  }
+
+  if (win) {
+    win.scrollTo(0, value)
+  } else {
+    elem.scrollTop = value
+  }
+}
+
+// check if the element is showing on container viewport
+export function isElementInViewport (elem, container = window, ahead = 1) {
+  const st = scrollTop(container) + offset(container).top
+  const ch = getClientHeight(container)
+  const elemTop = offset(elem).top
+  const elemHeight = elem.offsetHeight / ahead
+
+  return (elemTop + elemHeight > st && elemTop < st + ch)
+}
+
+export function scrollTo (target, element = window, speed = 0.3) {
+  // target value must less than the max value of scroll top and
+  // more than the min value of 0
+  target = Math.max(0, Math.min(getScrollHeight(element) - getClientHeight(element), target))
+
+  clearInterval(element.TIMER_OF_SRCOLL_TO)
+  element.TIMER_OF_SRCOLL_TO = setInterval(function () {
+    let st = scrollTop(element)
+    let position = (target - st) * speed
+
+    st += (st > target ? Math.floor(position) : Math.ceil(position))
+
+    scrollTop(element, st)
+
+    if (st < target + 1 && st > target - 1) {
+      clearInterval(element.TIMER_OF_SRCOLL_TO)
+      scrollTop(element, target)
+    }
+  }, 30)
+}
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,29 +1,29 @@
-export noop = () => {};
+export function noop () {}
 
-export function trimAll(string) {
-    return string.replace(/[\s\b]/g, '');
+export function trimAll (string) {
+  return string.replace(/[\s\b]/g, '')
 }
 
-export function getSign(urlString) {
-    return urlString.indexOf('?') === -1 ? '?' : '&';
+export function getSign (urlString) {
+  return urlString.indexOf('?') === -1 ? '?' : '&'
 }
 
 // Translation: {a: 1, b: 2} => '&a=1&b=2'
-export function objQueryStringify(obj) {
-    let str = '';
-    for (const key in obj) {
-        str += `&${key}=${encodeURIComponent(obj[key])}`;
-    }
-    return str;
+export function stringify (obj) {
+  let str = ''
+  for (const key in obj) {
+    str += `&${key}=${encodeURIComponent(obj[key])}`
+  }
+  return str
 }
 
-export function defineReadOnlyProperty(target, prop, value) {
-    Object.defineProperty(target, prop, {
-        value,
-        writable: false,
-        enumerable: true,
-        configurable: false
-    });
+export function defineReadOnlyProperty (target, prop, value) {
+  Object.defineProperty(target, prop, {
+    value,
+    writable: false,
+    enumerable: true,
+    configurable: false
+  })
 }
 
 /**
@@ -35,140 +35,38 @@ export function defineReadOnlyProperty(target, prop, value) {
  * @param digits {number} 显示位数（保留几位小数）
  * @return value {string} 格式后的值
  */
-export function formatAmountInput(value, digits) {
+export function formatAmountInput (value, digits) {
+  if (value !== '') {
+    // '.' => ''
+    // '2c' => '2'
+    value = value.replace(/^\.|[^\d.]/g, '')
     if (value !== '') {
-
-        // '.' => ''
-        // '2c' => '2'
-        value = value.replace(/^\.|[^\d.]/g, '');
-        if (value !== '') {
-            if (value.charAt(0) === '0' && value.charAt(1) !== '.') {
-
-                // '09' => '9'
-                // '00' => '0'
-                value = !+value ? '0' : value.substring(1);
+      if (value.charAt(0) === '0' && value.charAt(1) !== '.') {
+        // '09' => '9'
+        // '00' => '0'
+        value = !+value ? '0' : value.substring(1)
+      } else {
+        const tempValue = value.split('.')
+        if (tempValue.length >= 2) {
+          if (typeof digits === 'number') {
+            // eg(digits = 2): '12.300' => '12.30'
+            if (digits) {
+              let decimals = tempValue[1].substring(0, digits)
+              value = tempValue[0] + '.' + decimals
             } else {
-                const tempValue = value.split('.');
-                if (tempValue.length >= 2) {
-                    if (typeof digits === 'number') {
-
-                        // eg(digits = 2): '12.300' => '12.30'
-                        if (digits) {
-                            let decimals = tempValue[1].substring(0, digits);
-                            value = tempValue[0] + '.' + decimals;
-                        } else {
-
-                            // '12.' => '12'
-                            value = tempValue[0];
-                        }
-                    } else {
-
-                        // '12.30.' => '12.30'
-                        // '12..' => '12.'
-                        value = tempValue[0] + '.' + tempValue[1];
-                    }
-                }
+              // '12.' => '12'
+              value = tempValue[0]
             }
+          } else {
+            // '12.30.' => '12.30'
+            // '12..' => '12.'
+            value = tempValue[0] + '.' + tempValue[1]
+          }
         }
-    }
-    return value;
-}
-
-// 检查元素是否在某个元素里，与 jQuery.contains 等同
-export function contains(container, target) {
-    if (target) {
-        while (target = target.parentNode) {
-            if (container === target) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-// 从DOM中移除某个元素
-export function removeElement(selector) {
-    const elem = document.querySelector(selector);
-    elem && elem.parentNode.removeChild(elem);
-}
-
-export function isWindow (elem) {
-  return elem != null && elem === elem.window
-}
-
-export function offset (elem) {
-  const offset = { left: 0, top: 0 }
-  if (!isWindow(elem) && elem.nodeType !== 9) {
-    while (elem) {
-      offset.left += elem.offsetLeft
-      offset.top += elem.offsetTop
-      elem = elem.offsetParent
+      }
     }
   }
-  return offset
-}
-
-export function getClientHeight (elem) {
-  if (isWindow(elem) || elem.nodeType === 9) {
-    elem = document.documentElement
-  }
-  return elem.clientHeight
-}
-
-export function getScrollHeight (elem) {
-  if (isWindow(elem) || elem.nodeType === 9) {
-    elem = document.documentElement
-  }
-  return elem.scrollHeight
-}
-
-export function scrollTop (elem, value) {
-  let win
-  if (isWindow(elem)) {
-    win = elem
-  } else if (elem.nodeType === 9) {
-    win = elem.defaultView
-  }
-  
-  if (value === undefined) {
-    return win ? win.pageYOffset : elem.scrollTop
-  }
-  
-  if (win) {
-    win.scrollTo(0, value)
-  } else {
-    elem.scrollTop = value
-  }
-}
-
-// 检查元素是否在可视区内
-export function isElementInViewport(elem, container = winodw, ahead = 1) {
-  const st = scrollTop(container) + offset(container).top
-  const ch = getClientHeight(container)
-  const elemTop = offset(elem).top
-  const elemHeight = elem.offsetHeight / ahead
-  
-  return (elemTop + elemHeight > st && elemTop < st + ch)
-}
-
-export function scrollTo (to, element = window, speed = 0.3) {
-  // to 不能小于 0，不能大于最大滚动距离
-  to = Math.max(0, Math.min(getScrollHeight(element) - getClientHeight(element), to))
-  
-  clearInterval(element.TIMER_OF_SRCOLL_TO)
-  element.TIMER_OF_SRCOLL_TO = setInterval(function () {
-    let st = scrollTop(element)
-    let position = (to - st) * speed
-    
-    st += (st > to ? Math.floor(position) : Math.ceil(position))
-    
-    scrollTop(element, st)
-    
-    if (st < to + 1 && st > to - 1) {
-      clearInterval(element.TIMER_OF_SRCOLL_TO)
-      scrollTop(element, to)
-    }
-  }, 30)
+  return value
 }
 
 /**
@@ -178,90 +76,25 @@ export function scrollTo (to, element = window, speed = 0.3) {
  * @param  {Number}   must  可选，值 >= 0
  * @return {Function}       run when trigger it's event
  */
-export function throttle(handler, delay, must){
-    if (!delay && !must) {
-        return handler;
+export function throttle (handler, delay, must) {
+  if (!delay && !must) {
+    return handler
+  }
+
+  let startTime = new Date()
+  let timer
+
+  return function (e) {
+    if (delay) {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        handler.call(this, e)
+      }, delay)
+    } else if (new Date() - startTime > must) {
+      startTime = new Date()
+      handler.call(this, e)
     }
-
-    let startTime = new Date();
-    let timer;
-
-    return function (e) {
-        if (delay) {
-            clearTimeout(timer);
-            timer = setTimeout(() => {
-
-                handler.call(this, e);
-
-            }, delay);
-
-        } else if (new Date() - startTime > must) {
-            startTime = new Date();
-            handler.call(this, e);
-        }
-    }
-}
-
-export function loadScript(url) {
-    // jump url has been requested
-    if (loadScript.scripts.indexOf(url) !== -1) {
-        return Promise.resolve();
-    }
-    return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.onload = () => {
-            loadScript.scripts.push(url);
-            resolve();
-        };
-        script.onerror = (err) => {
-            reject(err);
-        };
-        script.src = url;
-        document.getElementsByTagName('head')[0].appendChild(script);
-    });
-}
-loadScript.scripts = [];
-
-/**
- * JSONP 跨域加载
- * @param url
- * @returns {Promise}
- *
- * Example：
- *  jsonp('http://domain.com/some-url')
- *  .then(() => {
- *    console.log('loading success')
- *  })
- *  .catch((err) => {
- *    console.log(err)
- *  })
- */
-export function jsonp(url) {
-    const id = '_utilities_jsonp_' + Date.now();
-    url += `${getSign(url)}callback=${id}`;
-
-    return new Promise((resolve, reject) => {
-        window[id] = (res) => {
-            jsonp.clearRemains(id);
-            resolve(res);
-        };
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.id = id;
-        script.onerror = (err) => {
-            jsonp.clearRemains(id);
-            reject(err);
-        };
-        script.src = url;
-        document.getElementsByTagName('head')[0].appendChild(script);
-    });
-}
-jsonp.clearRemains = (id) => {
-    removeElement(`#${id}`);
-    delete window[id];
+  }
 }
 
 /**
@@ -270,46 +103,46 @@ jsonp.clearRemains = (id) => {
  * extend( [ deep ,] target, object1 [, objectN ] )
  * @returns {object}
  */
-export function extend() {
-    const isArray = Array.isArray;
-    let arg = arguments,
-        target = arg[0] || {},
-        deep = false,
-        length = arg.length,
-        i = 1,
-        value, attr,
-        copyIsArray;
+export function extend () {
+  const isArray = Array.isArray
+  let arg = arguments,
+    target = arg[0] || {},
+    deep = false,
+    length = arg.length,
+    i = 1,
+    value, attr,
+    copyIsArray
 
-    if (typeof target === 'boolean') {
-        deep = target;
-        target = arg[1] || {};
-        i++;
-    }
+  if (typeof target === 'boolean') {
+    deep = target
+    target = arg[1] || {}
+    i++
+  }
 
-    for (; i < length; i++) {
-        for (attr in arg[i]) {
+  for (; i < length; i++) {
+    for (attr in arg[i]) {
+      value = arg[i][attr]
 
-            value = arg[i][attr];
+      if (deep && (isPlainObject(value) || (copyIsArray = isArray(value)))) {
+        let src = target[attr]
 
-            if (deep && (isPlainObject(value) || (copyIsArray = isArray(value)))) {
-
-                let src = target[attr];
-
-                if (copyIsArray) {
-                    copyIsArray = false;
-                    src = isArray(src) ? src : [];
-                } else {
-                    src = isPlainObject(src) ? src : {};
-                }
-
-                target[attr] = extend(deep, src, value);
-
-            } else {
-                target[attr] = value;
-            }
-
+        if (copyIsArray) {
+          copyIsArray = false
+          src = isArray(src) ? src : []
+        } else {
+          src = isPlainObject(src) ? src : {}
         }
-    }
 
-    return target;
+        target[attr] = extend(deep, src, value)
+      } else {
+        target[attr] = value
+      }
+    }
+  }
+
+  return target
+}
+
+export function uniqueRandom () {
+  return Date.now() + Math.random().toString(36).substring(2, 15)
 }

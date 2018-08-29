f29c32c0b6f60b71f9909651749d85803fccebce
Barrior <barrior@qq.com>
2018-08-29 19:37:27 +0800

    learn Mongoose & MongoDB

--- node.js/mongoose/schema_default_options.js
+++ node.js/mongoose/schema_default_options.js
@@ -10,7 +10,14 @@ const schemeOptions = {
   _id: false,
 
   // 禁用版本（ __v ）字段
-  versionKey: false
+  versionKey: false,
+
+  // 为文档添加时间戳，默认为 createdAt 和 updatedAt 字段
+  // 通过下面方式修改字段名称
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
 }
 
 const schema = new mongoose.Schema({
@@ -22,11 +29,7 @@ const Cat = mongoose.model('Cat', schema)
 
 ;(async function start () {
   try {
-    const tom = await Cat.create({ name: 'Tom' })
-    console.log(tom)
-
-    tom.age = 2
-    await tom.save()
+    const tom = await Cat.findOne({ name: 'Tom' })
     console.log(tom)
   } catch (e) {
     console.log('err: ', e)

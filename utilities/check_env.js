98aeb0cc1c0102acc2d65e56b238cc1b7105cdfb
Barrior <barrior@qq.com>
2019-04-16 17:50:43 +0800

    update check_env.js

--- utilities/check_env.js
+++ utilities/check_env.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 
 const allowENV = ['development', 'test', 'production']
 
-function checkEnv (scriptName) {
+function checkEnv () {
   const ENV = process.env.NODE_ENV
 
   if (!ENV) {
@@ -11,8 +11,10 @@ function checkEnv (scriptName) {
     ------------执行脚本请加上环境变量----------------
     使用示例：
 
-    NODE_ENV=production node scripts/${scriptName}
+      NODE_ENV=production node path/to/the/script
+    
     可选环境变量如下：
+    
       ${allowENV}
     `)
     process.exit(0)
@@ -20,13 +22,14 @@ function checkEnv (scriptName) {
 
   if (!includes(allowENV, ENV)) {
     console.log(`
-    选择的环境变量不是一个有效的环境变量
+    --------选择的环境变量不是一个有效的环境变量--------
     可选环境变量如下：
+    
       ${allowENV}
     `)
     process.exit(0)
   } else {
-    console.log(chalk.blue(`Current NODE_ENV: ${ENV}`))
+    console.log(chalk.blue(`当前环境变量 NODE_ENV: ${ENV}`))
   }
 }
 

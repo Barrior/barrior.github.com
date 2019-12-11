10383de32b12d962817ff461805b51a439bdfcd5
Barrior <barrior@qq.com>
2019-12-11 17:41:52 +0800

    remove nest files

--- node.js/nestjs/src/user/user.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-
-@Module({
-  imports: [],
-  controllers: [UserController],
-  // providers: [AppService],
-})
-export class UserModule {}

425e64d79076420ced7534333c3b653fe7c8684a
Barrior <barrior@qq.com>
2019-12-18 09:56:15 +0800

    fix layout position in chrome for "typescriptlang.org/docs/handbook"

---
+++ xdemo/ts-handbook.js
@@ -0,0 +1,5 @@
+(function () {
+  const contentElement = document.querySelector('.doc-content-container')
+  contentElement.classList.remove('col-md-10')
+  contentElement.classList.add('col-md-9')
+})()

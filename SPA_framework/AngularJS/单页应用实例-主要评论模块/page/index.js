e77303f5f21c604b5fbc8bda4562e932be103760
Barrior <barrior@qq.com>
2019-12-30 19:52:00 +0800

    remove SPA_framework directory

--- SPA_framework/AngularJS/单页应用实例-主要评论模块/page/index.js
+++
@@ -1,8 +0,0 @@
-let angular = require('angular');
-let app = angular.module('pageIndex', []);
-
-app.controller('ctrlIndex', ['$scope', ( $scope ) => {
-
-    $scope.info = ' - 我是首页控制器';
-
-}]);
--- SPA_framework/AngularJS/单页应用实例-主要评论模块/page/invoice.js
+++
@@ -1,8 +0,0 @@
-let angular = require('angular');
-let app = angular.module('pageInvoice', []);
-
-app.controller('ctrlInvoice', ['$scope', ( $scope ) => {
-
-    $scope.info = ' - 我是发票管理控制器';
-
-}]);
